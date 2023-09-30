30a8904e3ed43ffff4d4efd4e39042a6dd05d1ba
Luka <lukhanyo95@gmail.com>
2023-09-30 18:49:17 +0200

    verify email and password

--- src/controllers/user_control.js
+++ src/controllers/user_control.js
@@ -3,18 +3,23 @@ const bcrypt = require("bcryptjs");
 
 exports.register = async (req, res) => {
   const hash = bcrypt.hashSync(req.body.password, 10);
+  const hash2 = bcrypt.hashSync(req.body.password, 10);
+
+  const checkEmail = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/g.test(req.body.email);
   try {
     const user = new userModel({
       username: req.body.username,
       email: req.body.email,
       password: hash,
+      confirmPassword: hash2,
     });
 
-    const registeredUser = await user.save();
-
-    res
-      .status(201)
-      .json({ message: "Registered successfully!", user: registeredUser });
+    if (req.body.password === req.body.confirmPassword && checkEmail) {
+      const registeredUser = await user.save();
+      res
+        .status(201)
+        .json({ message: "Registered successfully!", user: registeredUser });
+    }
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
